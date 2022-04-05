fecec3a379f3899434665eaa11d2998b671975a6
Korobskoy Roman <korobskoyra@gmail.com>
2022-04-05 20:54:47 +0300

    added network error alert

---
+++ SpaceXInfo/Views/ShowAlert.swift
@@ -0,0 +1,17 @@
+//
+//  ShowAlert.swift
+//  SpaceXInfo
+//
+//  Created by Roman Korobskoy on 05.04.2022.
+//
+
+import Foundation
+import UIKit
+
+func showAlert(title: String, message: String, controller: UIViewController) {
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let okAction = UIAlertAction(title: "OK", style: .default)
+    
+    alert.addAction(okAction)
+    alert.present(controller, animated: true, completion: nil)
+}
