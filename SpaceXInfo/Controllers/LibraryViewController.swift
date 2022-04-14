48c18f4b9ae5240140d0b3d2e56dc625a17895a9
Korobskoy Roman <korobskoyra@gmail.com>
2022-04-14 17:34:50 +0300

    added tabbar controller

---
+++ SpaceXInfo/Controllers/LibraryViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryViewController.swift
+//  SpaceXInfo
+//
+//  Created by Roman Korobskoy on 14.04.2022.
+//
+
+import UIKit
+
+class LibraryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .mainBlue()
+    }
+}
---
+++ SpaceXInfo/Controllers/MainTabBarController.swift
@@ -0,0 +1,39 @@
+//
+//  MainTabBarController.swift
+//  SpaceXInfo
+//
+//  Created by Roman Korobskoy on 14.04.2022.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.tintColor = .mainWhite()
+        tabBar.barTintColor = .mainBlue()
+        tabBar.unselectedItemTintColor = .mainGray()
+        
+        let mainVC = MainViewController()
+        let libraryVC = LibraryViewController()
+        
+        let boldConfig = UIImage.SymbolConfiguration(weight: .medium)
+        let launchImage = UIImage(systemName: "photo.artframe", withConfiguration: boldConfig)
+        let libraryImage = UIImage(systemName: "square.and.arrow.down.on.square", withConfiguration: boldConfig)
+        
+        viewControllers = [generateNavigationController(rootViewController: mainVC, title: "Launches", image: launchImage!),
+                           generateNavigationController(rootViewController: libraryVC, title: "Favorites", image: libraryImage!)]
+    }
+    
+    private func generateNavigationController(rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.navigationBar.barTintColor = .mainBlue()
+        navigationVC.navigationBar.tintColor = .mainWhite()
+        navigationVC.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.mainWhite()]
+        navigationVC.tabBarItem.title = title
+        navigationVC.tabBarItem.image = image
+        
+        return navigationVC
+    }
+}
